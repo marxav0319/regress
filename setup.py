fabba0b07756dafbc3f0f84a9601de53d3772543
Mark Xavier <mark.j.xavier@gmail.com>
2024-03-24 22:36:52 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="regress",
+    version="0.1",
+    description="A small library to print a progress bar.",
+    author="Mark Xavier",
+    author_email="mark.j.xavier@gmail.com",
+    packages=["regress"],
+
+)
