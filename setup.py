3daf6581530f5f73f5edbe9669c04921f24c32ab
Mark Xavier <mark.j.xavier@gmail.com>
2024-03-26 02:15:44 -0400

    Correct version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="regress",
-    version="0.1",
+    version="0.0.1",
     description="A small library to print a progress bar.",
     author="Mark Xavier",
     author_email="mark.j.xavier@gmail.com",
